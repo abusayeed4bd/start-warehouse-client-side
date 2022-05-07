b0c04e6463f8b82183c7f15090943523b51965a3
abusayeed4bd <abusayeed4bd@gmail.com>
2022-05-08 00:13:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFouond from './pages/NotFound/NotFouond';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <h1 className="text-danger">this is from app.js </h1>
+      <button className='btn btn-danger'>click here</button>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='*' element={<NotFouond></NotFouond>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link className="fw-bold" as={Link} to="/">Home</Nav.Link>
+        <Nav.Link as={Link} to="/services">Services</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    );
+};
+
+export default Header;
