8982334458d8c3043ab2ec26098a70613f2ca4fe
abusayeed4bd <abusayeed4bd@gmail.com>
2022-05-08 17:54:44 +0600

    nav bar modification

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <Navbar  className="sticky-top" bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link className="fw-bold" as={Link} to="/">Home</Nav.Link>
-        <Nav.Link as={Link} to="/services">Services</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-    );
+  return (
+    <Navbar className="sticky-top" bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="fw-bold" as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Button
+              className="btn btn-warning text-white"
+              as={Link}
+              to="/login"
+            >
+              Login &#9094;
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
