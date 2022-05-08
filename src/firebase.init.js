c1aeed5576ce565fc0aa6343f5db347a339b9a67
abusayeed4bd <abusayeed4bd@gmail.com>
2022-05-08 22:02:37 +0600

    register page functionality complate

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {useAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,11 +11,11 @@ const firebaseConfig = {
   projectId: "react-node-mongo-warehouse",
   storageBucket: "react-node-mongo-warehouse.appspot.com",
   messagingSenderId: "876033744364",
-  appId: "1:876033744364:web:0bbe31fdbfcb4a54d574c9"
+  appId: "1:876033744364:web:0bbe31fdbfcb4a54d574c9",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = useAuth(app);
+const auth = getAuth(app);
 export default auth;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleEmailOnBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordOnBlur = (event) => {
+    setPassword(event.target.value);
+  };
   const handleCreateAccount = (event) => {
     event.preventDefault();
+    createUserWithEmailAndPassword(email, password);
   };
+
   return (
     <div>
       <div className="w-50 mx-auto bg-light p-4 my-3">
@@ -14,14 +31,24 @@ const Register = () => {
         <Form onSubmit={handleCreateAccount}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Control
+              onBlur={handleEmailOnBlur}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" required />
+            <Form.Control
+              onBlur={handlePasswordOnBlur}
+              type="password"
+              placeholder="Password"
+              required
+            />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
               type="password"
@@ -31,8 +58,16 @@ const Register = () => {
           </Form.Group>
 
           <Button className="w-100 text-white" variant="warning" type="submit">
-            Create Account
+            {loading ? "Loading..." : "Create Account"}
           </Button>
+          {error && (
+            <p className="text-danger text-center my-2">{error.message}</p>
+          )}
+          {user && (
+            <p className="text-success text-center my-2">
+              Account Created Successfully
+            </p>
+          )}
         </Form>
         <div>
           <p className="text-center my-4">
